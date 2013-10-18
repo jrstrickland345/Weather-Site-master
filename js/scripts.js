f64642de1305c9f15b7a8a49c33ce5fb2dfd715b
jrstrickland345 <graphicdesign@Graphics-iMac-10.local>
2013-10-18 14:38:04 -0700

    added weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
+	
+	//Button Clicked
 	$('button').on('click', function(){
 		console.log('You clicked the button');
 		var zip = $('input').val();
 		console.log(zip);
+
+		$.simpleWeather({
+		zipcode: zip,
+		success: function(weather){
+			html = '<h1>' + weather.temp + '</h1>';
+			html += '<h2>' + weather.city + '</h2>';
+			html += '<img src="' + weather.tomorrow.image + '">';
+			console.log(weather.temp);
+
+			$('#weather').html(html);
+		}
 	});
+
+	
+	});
+
+	//Get Weather
+
+	
+
+	//Reset Text Field
 	$('input:text').click(function(){
 		$(this).val('');
 	});

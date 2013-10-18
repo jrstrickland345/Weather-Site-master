95f9cce24d351a6460c0c40830e2f84a568b8fe4
jrstrickland345 <graphicdesign@Graphics-iMac-10.local>
2013-10-18 13:58:33 -0700

    added clear input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	$('button').on('click', function(){
 		console.log('You clicked the button');
-		$('body').hide();
+		var zip = $('input').val();
+		console.log(zip);
+	});
+	$('input:text').click(function(){
+		$(this).val('');
 	});
 });
